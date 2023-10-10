d386cfe77580a88e0526152c66da8e8889681d11
AlexShinalov <snw0987@gmail.com>
2023-10-10 14:04:40 +0300

    new_filters

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import cv2
 import numpy as np
 
 #резкость
-def apply_sharpening_filter(input_path, output_path):
+def blure_filter(input_path, output_path):
     image = cv2.imread(input_path)
-    kernel = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
-    sharpened = cv2.filter2D(image, -1, kernel)
-    cv2.imwrite(output_path, sharpened)
+    blurred = cv2.blur(image, (5, 5))
+    cv2.imwrite(output_path, blurred)
 
 #степень
-def apply_sepia_filter(input_path, output_path):
+def sepia_filter(input_path, output_path):
     image = cv2.imread(input_path)
     kernel = np.array([[0.272, 0.534, 0.131],
                        [0.349, 0.686, 0.168],
@@ -20,10 +19,10 @@ def apply_sepia_filter(input_path, output_path):
     cv2.imwrite(output_path, sepia_toned)
 
 #уменьшение размера
-def resize_image(input_path, output_path, width, height):
+def bandw_image(input_path, output_path, width, height):
     image = cv2.imread(input_path)
-    resized = cv2.resize(image, (width, height))
-    cv2.imwrite(output_path, resized)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(output_path, gray)
 
 
 def process_images(input_folder, output_folder):
@@ -32,21 +31,21 @@ def process_images(input_folder, output_folder):
             input_path = os.path.join(input_folder, filename)
 
 
-            sharpened_output_path = os.path.join(output_folder, f'sharpened_{filename}')
+            blure_output_path = os.path.join(output_folder, f'sharpened_{filename}')
             sepia_output_path = os.path.join(output_folder, f'sepia_{filename}')
-            resized_output_path = os.path.join(output_folder, f'resized_{filename}')
+            bandw_output_path = os.path.join(output_folder, f'resized_{filename}')
 
-            sharpening_thread = threading.Thread(target=apply_sharpening_filter, args=(input_path, sharpened_output_path))
-            sepia_thread = threading.Thread(target=apply_sepia_filter, args=(input_path, sepia_output_path))
-            resize_thread = threading.Thread(target=resize_image, args=(input_path, resized_output_path, 800, 600))
+            blure_thread = threading.Thread(target=blure_filter, args=(input_path, blure_output_path))
+            sepia_thread = threading.Thread(target=sepia_filter, args=(input_path, sepia_output_path))
+            bandw_thread = threading.Thread(target=bandw_image, args=(input_path, bandw_output_path, 800, 600))
 
-            sharpening_thread.start()
+            blure_thread.start()
             sepia_thread.start()
-            resize_thread.start()
+            bandw_thread.start()
 
-            sharpening_thread.join()
+            blure_thread.join()
             sepia_thread.join()
-            resize_thread.join()
+            bandw_thread.join()
 
 
 input_folder = 'C:/Users/snw12/PycharmProjects/FP-1/foto'
